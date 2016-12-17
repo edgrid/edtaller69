25cc2bbe123eddb67e2a9a4e95e278ef689f9e06
Alvaro Felipe <alvaro@escuela.digital>
2016-12-17 12:58:09 -0500

    edtaller 70 se avanzo con el proyecto hasta la tabla de precios y se anadio sticky menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 edgrid.menu('main-nav','main-menu');
+
+
+(function(d){
+  let nav = d.getElementById('nav-container');
+  let top = nav.offsetTop;
+  window.addEventListener('scroll', () => {
+    let scroll = d.body.scrollTop;
+    if (scroll >= top) {
+      nav.classList.add('sticky');
+    } else {
+      nav.classList.remove('sticky');
+    }
+  });
+})(document);
